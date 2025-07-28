cae9a52208c3f522118746bfcb93a97f1237b7fd
Elise Palethorpe <ellypaley@gmail.com>
2025-07-28 23:08:53 +0000

    hello pi

--- OMP_CPU_Exercises/hello.c
+++ OMP_CPU_Exercises/hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main ()  
-{
-  printf("Hello World \n");
+#include <omp.h>
+
+int main() {
+  omp_set_num_threads(40);
+#pragma omp parallel
+  {
+    printf("Hello World from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  }
 }
--- OMP_CPU_Exercises/pi.c
+++ OMP_CPU_Exercises/pi.c
@@ -14,28 +14,43 @@ History: Written by Tim Mattson, 11/99.
 
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 static long num_steps = 100000000;
 double step;
-int main ()
+int main (int argc, char* argv[])
 {
+    int nthreads = atoi(argv[1]);
 	  int i;
 	  double x, pi, sum = 0.0;
 	  double start_time, run_time;
 
 	  step = 1.0/(double) num_steps;
 
-        	 
+    omp_set_num_threads(nthreads);
 	  start_time = omp_get_wtime();
 
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
-	  }
+    double sums[nthreads];
+
+#pragma omp parallel 
+  {
+    int thread_id = omp_get_thread_num();
+    int num_threads = omp_get_num_threads();
+    // race condition
+    if (thread_id == 0) nthreads = num_threads;
+    sums[thread_id] = 0;
+    for (i = thread_id + 1; i <= num_steps; i+=nthreads) {
+        x = (i-0.5)*step;
+        sums[thread_id] += 4.0/(1.0+x*x);
+    }
+  }
+    for (i = 0; i < nthreads; i++) {
+        sum += sums[i];
+    }
 
 	  pi = step * sum;
 	  run_time = omp_get_wtime() - start_time;
-	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
+	  printf("pi with %ld steps and %d threads is %lf in %lf seconds\n",num_steps, nthreads, pi, run_time);
 }	  
 
 
